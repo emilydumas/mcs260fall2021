a6e7295963d7cad1a1e66998be65af33467ce435
David Dumas <david@dumas.io>
2021-08-28 17:30:12 -0500

    Lecture 3 scripts

---
+++ samplecode/greet.py
@@ -0,0 +1,5 @@
+# MCS 260 Fall 2021 Lecture 3
+# Greet the user by name
+
+username = input("Name? ")
+print("Howdy ",username,"!",sep="")
---
+++ samplecode/rectangle.py
@@ -0,0 +1,8 @@
+# MCS 260 Fall 2021 Lecture 3
+# Compute area and perimeter of a rectangle
+
+width = float(input("Enter width: "))
+height = float(input("Enter height: "))
+
+print("Area of rectangle is",width*height)
+print("Perimeter of the rectangle is",2*(width+height))
