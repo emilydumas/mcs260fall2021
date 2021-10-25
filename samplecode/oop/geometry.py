b944a13278da67e97bdaeaaf0a76835f1cf7692d
David Dumas <david@dumas.io>
2021-10-25 09:45:53 -0500

    Lecture 26: __eq__, __str__, and __repr__ methods

--- samplecode/oop/geometry.py
+++ samplecode/oop/geometry.py
@@ -12,10 +12,35 @@ class Rectangle:
         self.cy = cy
         self.w = w
         self.h = h
+    def __str__(self):
+        "Return a string that represents the object"
+        return "Rectangle(cx={},cy={},w={},h={})".format(
+            self.cx,
+            self.cy,
+            self.w,
+            self.h,
+        )
+    def __repr__(self):
+        """Return a string that represents the object, when it is
+        necessary to display it IN THE PYTHON REPL"""
+        # Let's just call the __str__ method we already defined!
+        return self.__str__()
+    def __eq__(self,other):
+        """Return True if Rectangle objects self and other have the
+        same center, width, and height"""
+        if isinstance(other,Rectangle):
+            return self.cx==other.cx and self.cy==other.cy and self.w==other.w and self.h==other.h
+        else:
+            # The other thing isn't a rectangle
+            return False
     def scale(self,factor):
         "Modify the rectangle, multiplying its w and h by `factor`"
         self.w *= factor
         self.h *= factor
+    def translate(self,dx,dy):
+        "Translate the rectangle by vector (dx,dy)."
+        self.cx += dx
+        self.cy += dy
 
 class Circle:
     """A circle in the plane represented by its center (cx,cy) and
@@ -25,7 +50,26 @@ class Circle:
         self.cx = cx  # create a new attribute called cx
         self.cy = cy
         self.r = r
+    def __str__(self):
+        "Return a string that represents the object"
+        return "Circle(cx={},cy={},r={})".format(
+            self.cx,
+            self.cy,
+            self.r,
+        )
+    def __repr__(self):
+        """Return a string that represents the object, when it is
+        necessary to display it IN THE PYTHON REPL"""
+        # Let's just call the __str__ method we already defined!
+        return self.__str__()
+    def __eq__(self,other):
+        """Return True if Circle objects self and other have the
+        same center and radius."""
+        return self.cx==other.cx and self.cy==other.cy and self.r==other.r
     def scale(self,factor):
         "Modify the circle, multiplying its radius by `factor`"
         self.r *= factor
-
+    def translate(self,dx,dy):
+        "Translate the circle by vector (dx,dy)."
+        self.cx += dx
+        self.cy += dy
