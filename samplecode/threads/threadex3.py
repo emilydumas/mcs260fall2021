bb695190928bc5b3f4b692af738dae11957dbd0f
David Dumas <david@dumas.io>
2021-11-30 11:59:25 -0600

    Richer producer-consumer example (multiple threads)

---
+++ samplecode/threads/threadex3.py
@@ -0,0 +1,55 @@
+"""
+Producer-consumer example with multiple threads
+"""
+# MCS 260 Fall 2021 Lecture 39
+import threading
+import time
+import random
+import queue
+
+class WorkerThread(threading.Thread):
+    """
+    Thread that does work in the background
+    """
+    def __init__(self,Q,workernumber=0):
+        """
+        Make a new worker that receives its work from
+        a queue.Queue object Q
+        """
+        super().__init__()
+        self.workernumber = workernumber
+        self.Q = Q  # save Q so we can access it in .run()
+        self.idstr = "Worker {}".format(self.workernumber)
+
+    def run(self):
+        "Main work of the thread"
+        while True:
+            x = self.Q.get()
+            if x == None:
+                print(self.idstr,"shutting down on request")
+                break
+            print(self.idstr,"now working on {}".format(x),flush=True)
+            time.sleep(2*random.random())
+            print(self.idstr,"finished job {}".format(x),flush=True)
+
+jobqueue = queue.Queue()
+
+num_workers = 3
+num_jobs = 30
+
+print("Starting {} worker threads".format(num_workers))
+workers = [ WorkerThread(jobqueue,i) for i in range(num_workers) ]
+for T in workers:
+    T.start()
+
+for i in range(num_jobs):
+    print("Submit job {}".format(i),flush=True)
+    jobqueue.put(i)
+
+print("Adding None(s) to the queue so the worker quits when done")
+
+# We need to add `num_workers` copies of None to the queue so that
+# each thread gets one.  (Could handle this in other ways, e.g. by
+# setting an attribute of each thread that signals completion.)
+for _ in range(num_workers):
+    jobqueue.put(None)
